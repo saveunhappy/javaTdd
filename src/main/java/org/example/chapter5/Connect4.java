34549a529f7588f3cea14be827e9f66b5e48ffb1
JiangTaoHou <925147954@qq.com>
2023-07-12 20:06:40 +0800

    1.需求没有指定输出通道。出于简化考虑，我们决定使用系统标准输出以指出发生的事件。执行操作的每个方法都添加了几行提供输出的代码，让用户知道游戏的当前状态:

--- src/main/java/org/example/chapter5/Connect4.java
+++ src/main/java/org/example/chapter5/Connect4.java
@@ -1,37 +1,52 @@
 package org.example.chapter5;
 
 import java.util.Arrays;
+import java.util.StringJoiner;
 
 public class Connect4 {
     public enum Color {
         RED('R'), GREEN('G'), EMPTY(' ');
         private final char value;
-        Color(char value) { this.value = value; }
+
+        Color(char value) {
+            this.value = value;
+        }
+
         @Override
         public String toString() {
             return String.valueOf(value);
         }
     }
+
     //初始化，红色先下
     private Color currentPlayer = Color.RED;
+    private static final String DELIMITER = "|";
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
-    private Color[][] board = new Color[COLUMNS][ROWS];
+    private final Color[][] board = new Color[COLUMNS][ROWS];
+
     public Connect4() {
         for (Color[] column : board) {
             Arrays.fill(column, Color.EMPTY);
         }
     }
+
     //使用Color.RED指出哪些位置被占据了
     public void putDisc(int column) {
         if (column > 0 && column <= COLUMNS) {
-            int numOfDiscs =
-                    getNumberOfDiscsInColumn(column - 1);
+            int numOfDiscs = getNumberOfDiscsInColumn(column - 1);
             if (numOfDiscs < ROWS) {
-                board[column - 1][numOfDiscs] =
-                        currentPlayer;
-                switchPlayer();
+                board[column - 1][numOfDiscs] = currentPlayer;
+                printBoard();        // 4.打印
+                switchPlayer();        // 3.切换玩家
+            } else {                // 4.打印
+                System.out.println(numOfDiscs);
+                System.out.println("There's no room for a new disc in this column");
+                printBoard();
             }
+        } else {                    // 4.打印
+            System.out.println("Column out of bounds");
+            printBoard();
         }
     }
 
@@ -55,6 +70,16 @@ public class Connect4 {
             currentPlayer = Color.RED;
         }
         System.out.println("Current turn:" + currentPlayer);
+    }
+
+    public void printBoard() {
+        for (int row = ROWS - 1; row >= 0; row--) {
+            StringJoiner stringJoiner = new StringJoiner(DELIMITER, DELIMITER, DELIMITER);
+            for (int col = 0; col < COLUMNS; ++col) {
+                stringJoiner.add(board[col][row].toString());
+            }
+            System.out.println(stringJoiner);
+        }
     }
 
 
