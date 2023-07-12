6e516155b401bf16f2d16a5c308fae301085bb3e
JiangTaoHou <925147954@qq.com>
2023-07-12 20:13:16 +0800

    1.全部的代码

--- src/main/java/org/example/chapter5/Connect4.java
+++ src/main/java/org/example/chapter5/Connect4.java
@@ -2,6 +2,7 @@ package org.example.chapter5;
 
 import java.util.Arrays;
 import java.util.StringJoiner;
+import java.util.regex.Pattern;
 
 public class Connect4 {
     public enum Color {
@@ -24,7 +25,8 @@ public class Connect4 {
     public static final int COLUMNS = 7;
     public static final int ROWS = 6;
     private final Color[][] board = new Color[COLUMNS][ROWS];
-
+    private Color winner;
+    public static final int DISCS_FOUR_WIN = 4;
     public Connect4() {
         for (Color[] column : board) {
             Arrays.fill(column, Color.EMPTY);
@@ -36,15 +38,16 @@ public class Connect4 {
         if (column > 0 && column <= COLUMNS) {
             int numOfDiscs = getNumberOfDiscsInColumn(column - 1);
             if (numOfDiscs < ROWS) {
-                board[column - 1][numOfDiscs] = currentPlayer;
-                printBoard();        // 4.打印
-                switchPlayer();        // 3.切换玩家
-            } else {                // 4.打印
+                board[column-1][numOfDiscs] = currentPlayer;
+                printBoard();		// 4.打印
+                checkWinCondition(column-1, numOfDiscs);// 6.检查获胜
+                switchPlayer();		// 3.切换玩家
+            } else {				// 4.打印
                 System.out.println(numOfDiscs);
                 System.out.println("There's no room for a new disc in this column");
                 printBoard();
             }
-        } else {                    // 4.打印
+        } else {					// 4.打印
             System.out.println("Column out of bounds");
             printBoard();
         }
@@ -93,5 +96,51 @@ public class Connect4 {
         }
         return false;
     }
-
+    private void checkWinCondition(int col,int row) {
+        Pattern winPattern = Pattern.compile(".*" + currentPlayer + "{" + DISCS_FOUR_WIN + "}.*");
+        // 检查垂直方向
+        StringJoiner stringJoiner = new StringJoiner("");
+        for (int auxRow = 0; auxRow < ROWS; auxRow++) {
+            stringJoiner.add(board[col][auxRow].toString());
+        }
+        if (winPattern.matcher(stringJoiner.toString()).matches()) {
+            winner = currentPlayer;
+            System.out.println(currentPlayer + " wins");
+            return;
+        }
+        // 检查水平方向
+        stringJoiner = new StringJoiner("");
+        for (int column = 0; column < COLUMNS; ++column) {
+            stringJoiner.add(board[column][row].toString());
+        }
+        if (winPattern.matcher(stringJoiner.toString()).matches()) {
+            winner = currentPlayer;
+            System.out.println(currentPlayer + " wins");
+            return;
+        }
+        // 检查对角线方向(从右上角到左下角的对角线)
+        int startOffset = Math.min(col, row);
+        int column = col - startOffset, auxRow = row - startOffset;
+        stringJoiner = new StringJoiner("");
+        do {
+            stringJoiner.add(board[column++][auxRow++].toString());
+        } while (column < COLUMNS && auxRow < ROWS);
+        if (winPattern.matcher(stringJoiner.toString()).matches()) {
+            winner = currentPlayer;
+            System.out.println(currentPlayer + " wins");
+            return;
+        }
+        // 检查对角线方向(从右下角到左上角的对角线)
+        startOffset = Math.min(col, ROWS - 1 - row);
+        column = col - startOffset;
+        auxRow = row + startOffset;
+        stringJoiner = new StringJoiner("");
+        do {
+            stringJoiner.add(board[column++][auxRow--].toString());
+        } while (column < COLUMNS && auxRow >= 0);
+        if (winPattern.matcher(stringJoiner.toString()).matches()) {
+            winner = currentPlayer;
+            System.out.println(currentPlayer + " wins");
+        }
+    }
 }
