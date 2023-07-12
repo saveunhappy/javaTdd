5ec7647cd1864057773169bd0cfec12c8eaad85b
JiangTaoHou <925147954@qq.com>
2023-07-12 19:48:36 +0800

    1.玩家从列顶放入碟片。如果整列都是空的，放入的碟片将落到底部。在特定 列中，后放入的碟片将叠在前面放入的碟片之上

--- src/main/java/org/example/chapter5/Connect4.java
+++ src/main/java/org/example/chapter5/Connect4.java
@@ -19,4 +19,31 @@ public class Connect4 {
         for (Color[] column : board) {
             Arrays.fill(column, Color.EMPTY);
         }
-    } }
+    }
+    //使用Color.RED指出哪些位置被占据了
+    public void putDisc(int column) {
+        if (column > 0 && column <= COLUMNS) {
+            int numOfDiscs =
+                    getNumberOfDiscsInColumn(column - 1);
+            if (numOfDiscs < ROWS) {
+                board[column - 1][numOfDiscs] =
+                        Color.RED;
+            }
+        }
+    }
+
+    private int getNumberOfDiscsInColumn(int column) {
+        if (column >= 0 && column < COLUMNS) {
+            int row;
+            for (row = 0; row < ROWS; row++) {
+                if (Color.EMPTY == board[column][row]) {
+                    return row;
+                }
+            }
+            return row;
+        }
+        return -1;
+    }
+
+
+}
