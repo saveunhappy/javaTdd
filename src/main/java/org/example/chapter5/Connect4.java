86ce8191798b693f185a21ee6722f3fe002770ec
JiangTaoHou <925147954@qq.com>
2023-07-12 20:10:31 +0800

    1.无法再放入碟片时游戏结束，结果为平局。

--- src/main/java/org/example/chapter5/Connect4.java
+++ src/main/java/org/example/chapter5/Connect4.java
@@ -81,6 +81,17 @@ public class Connect4 {
             System.out.println(stringJoiner);
         }
     }
-
+    public boolean isFinished() {
+        int numOfDiscs = 0;
+        for (int col = 0; col < COLUMNS; ++col) {
+            numOfDiscs +=
+                    getNumberOfDiscsInColumn(col);
+        }
+        if (numOfDiscs >= COLUMNS * ROWS) {
+            System.out.println("It's a draw");
+            return true;
+        }
+        return false;
+    }
 
 }
